7cd341826b43d894446dc7285869b72b9918780e
Jinyao <lingerlyn@gmail.com>
2021-12-28 12:46:24 -0500

    pixel format is a big issue

--- src/video_capture.cpp
+++ src/video_capture.cpp
@@ -1,5 +1,6 @@
 #include "thread.h"
 #include "video_capture.h"
+#include <opencv2/opencv.hpp>
 
 void aquire_num_frames(Emergent::CEmergentCamera* camera, Emergent::CEmergentFrame* frame_recv, int num_frames)
 {
@@ -62,6 +63,86 @@ void aquire_num_frames(Emergent::CEmergentCamera* camera, Emergent::CEmergentFra
 }
 
 
+void aquire_and_display(Emergent::CEmergentCamera* camera, Emergent::CEmergentFrame* frame_recv, CameraParams camera_params)
+{
+    int camera_return {0};
+
+    unsigned int size_of_buffer;
+    size_of_buffer = frame_recv->CalculateBufferSize();
+    printf("Buffer size (bytes): \t%d\n ", size_of_buffer);
+
+    //aquisition
+    check_camera_errors(EVT_CameraExecuteCommand(camera, "AcquisitionStart"));
+
+    unsigned short id_prev = 0, dropped_frames = 0;
+    unsigned int frames_recd = 0;   
+    int frame_count {0};
+    float start_time = tick();
+        
+    while(true)
+    {
+        camera_return = EVT_CameraGetFrame(camera, frame_recv, EVT_INFINITE);
+        if(!camera_return)
+        {
+            //Counting dropped frames through frame_id as redundant check. 
+            if(((frame_recv->frame_id) != id_prev+1) && (frame_count != 0)) dropped_frames++;
+            else
+            {
+                frames_recd++;
+                // TODO: program what to do with received frame 
+                // using opencv or use opengl for color coversion for display? how does opencv draw windows? 
+                cv::Mat frame;
+                if(camera_params.pixel_format == "YUV422Packed")
+                {
+                    cv::Mat frameConvert(camera_params.height, camera_params.width, CV_8UC2, frame_recv->imagePtr);                
+                    cv::cvtColor(frameConvert, frame, cv::COLOR_YUV2BGR_Y422);
+                }
+                else if (camera_params.pixel_format == "BayerRG8")
+                {
+                    cv::Mat frameConvert(camera_params.height, camera_params.width, CV_8UC1, frame_recv->imagePtr);                
+                    cv::cvtColor(frameConvert, frame, cv::COLOR_BayerBG2BGR);
+                }
+                cv::resize(frame, frame, cv::Size(1604, 1100), 0, 0, cv::INTER_LINEAR);
+                cv::imshow("camera", frame);
+            }
+        }
+        else{dropped_frames++; printf("\nEVT_CameraGetFrame Error = %8.8x!\n", camera_return);}
+
+        //In GVSP there is no id 0 so when 16 bit id counter in camera is max then the next id is 1 so set prev id to 0 for math above.
+        if(frame_recv->frame_id == 65535)
+            id_prev = 0;
+        else
+            id_prev = frame_recv->frame_id;
+
+        if(camera_return)
+            break; //No requeue reqd
+
+        camera_return = EVT_CameraQueueFrame(camera, frame_recv); //Re-queue.
+        if(camera_return) printf("EVT_CameraQueueFrame Error!\n");
+
+        if(frame_count % 100 == 99) {printf("."); fflush(stdout);}    
+        if(frame_count % 10000 == 9999) printf("\n");
+
+        if(dropped_frames >= 100) break;
+        frame_count++;
+
+        char c = (char)cv::waitKey(1);
+        if (c==27)              
+            break; // ESC for quiting
+    }
+
+    float end_time = tick();
+    float time_diff = end_time - start_time;
+    check_camera_errors(EVT_CameraExecuteCommand(camera, "AcquisitionStop"));
+
+    //Report stats
+    printf("\n");
+    printf("Images Captured: \t%d\n", frames_recd);
+    printf("Dropped Frames: \t%d\n", dropped_frames);
+    printf("Calculated Frame Rate: \t%f\n", frames_recd/time_diff);
+}
+
+
 
 void aquire_and_encode_ffmpeg(Emergent::CEmergentCamera* camera, Emergent::CEmergentFrame* frame_recv, int num_frames, CameraParams camera_params, FILE* encoder_stream)
 {
@@ -88,7 +169,7 @@ void aquire_and_encode_ffmpeg(Emergent::CEmergentCamera* camera, Emergent::CEmer
             else
             {
                 frames_recd++;
-                // write to pipe
+                // pixel format is a big issue; write to pipe
                 fwrite(frame_recv->imagePtr, 1, size_of_buffer, encoder_stream);
             }
         }

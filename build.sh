4bd415c6e2c5ee8f45fc9ad2ae3fd5b435c5f3c0
Jinyao <lingerlyn@gmail.com>
2021-12-30 11:47:46 -0500

    human readable build

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 mkdir -p targets;
 rm -f targets/orange;
-g++ -Ofast -ffast-math -std=c++14 -o targets/video_capture.o src/video_capture.cpp -o targets/camera_driver_helper.o src/camera_driver_helper.cpp -o targets/camera.o src/camera.cpp -o targets/orange src/orange.cpp -I/usr/include/opencv4 -I/opt/EVT/eSDK/include/ -I/usr/lib/x86_64-linux-gnu/gstreamer-1.0 -L/opt/EVT/eSDK/lib/ -lEmergentCamera  -lEmergentGenICam  -lEmergentGigEVision -lm -lpthread -lopencv_core -lopencv_imgcodecs -lopencv_bgsegm -lopencv_imgproc -lopencv_video -lopencv_highgui -lopencv_videoio -lgstreamer-1.0; 
+g++ -Ofast -ffast-math -std=c++14 \
+    -o targets/video_capture.o src/video_capture.cpp \
+    -o targets/camera_driver_helper.o src/camera_driver_helper.cpp \
+    -o targets/camera.o src/camera.cpp -o targets/orange src/orange.cpp \
+    -I/usr/include/opencv4 -I/opt/EVT/eSDK/include/ -I/usr/lib/x86_64-linux-gnu/gstreamer-1.0 \
+    -L/opt/EVT/eSDK/lib/ -lEmergentCamera  -lEmergentGenICam  -lEmergentGigEVision \
+    -lm \
+    -lpthread \
+    -lopencv_core -lopencv_imgcodecs -lopencv_bgsegm -lopencv_imgproc -lopencv_video -lopencv_highgui -lopencv_videoio \
+    -lgstreamer-1.0; 
 sudo ./targets/orange;

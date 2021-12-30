33d5225147ed198b6afc0a571953e9531d926e20
Jinyao <lingerlyn@gmail.com>
2021-12-30 18:23:11 -0500

    hmm, need to build env

--- external/build_app_enc.sh
+++ external/build_app_enc.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 mkdir -p targets_nvenc;
-rm -f targets/AppEncCuda;
+rm -f targets_nvenc/AppEncCuda;
 g++ -Ofast -ffast-math -std=c++14 \
-    -o targets/AppEncCuda.o src/AppEncCuda.cpp \
+    -o targets_nvenc/NvEncoder.o NvEncoder/NvEncoder.cpp \
+    -o targets_nvenc/NvEncoder/NvEncoderCuda.o NvEncoder/NvEncoderCuda.cpp \
+    -o targets_nvenc/NvEncoder/NvEncoderOutputInVidMemCuda.o NvEncoder/NvEncoderOutputInVidMemCuda.cpp \
+    -o targets_nvenc/AppEncCuda.o AppEncCuda.cpp \
+    -I/usr/local/cuda-11.4/include -I/home/ash/nvenc_api/include -I/home/ash/src/orange/external/Utils -I/home/ash/src/orange/external/NvEncoder \
     -lm \
     -lpthread;
 sudo ./targets_nvenc/AppEncCuda;

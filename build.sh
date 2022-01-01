fe029377ff4218124f0b15daad0a19dd3dd05710
Jinyao <lingerlyn@gmail.com>
2022-01-01 15:58:35 -0500

    optimize g++ build function

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir -p targets;
 rm -f targets/orange;
 g++ -Ofast -ffast-math -std=c++14 \
-    -o targets/orange src/orange.cpp -I ./src/ ./src/*.cpp \
+    -o targets/orange -I ./src/ ./src/*.cpp \
     -I/usr/include/opencv4 -I/opt/EVT/eSDK/include/ -I/usr/lib/x86_64-linux-gnu/gstreamer-1.0 \
     -L/opt/EVT/eSDK/lib/ -lEmergentCamera  -lEmergentGenICam  -lEmergentGigEVision \
     -lm \

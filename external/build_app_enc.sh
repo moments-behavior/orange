4844ba85e9c4f5752959b4e04c94fa53c0750d13
Jinyao <lingerlyn@gmail.com>
2022-01-01 15:47:43 -0500

    compile cuda program

--- external/build_app_enc.sh
+++ external/build_app_enc.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 mkdir -p targets_nvenc;
 rm -f targets_nvenc/AppEncCuda;
-g++ -Ofast -ffast-math -std=c++14 \
-    -o targets_nvenc/NvEncoder.o NvEncoder/NvEncoder.cpp \
-    -o targets_nvenc/NvEncoder/NvEncoderCuda.o NvEncoder/NvEncoderCuda.cpp \
-    -o targets_nvenc/NvEncoder/NvEncoderOutputInVidMemCuda.o NvEncoder/NvEncoderOutputInVidMemCuda.cpp \
-    -o targets_nvenc/AppEncCuda.o AppEncCuda.cpp \
-    -I/usr/local/cuda-11.4/include -I/home/ash/nvenc_api/include -I/home/ash/src/orange/external/Utils -I/home/ash/src/orange/external/NvEncoder \
-    -lm \
-    -lpthread;
-sudo ./targets_nvenc/AppEncCuda;
+nvcc -c -arch=sm_80 Utils/*.cu -odir targets_nvenc
+g++ -Ofast -ffast-math -std=c++14 -o targets_nvenc/*.o -o targets_nvenc/AppEncCuda AppEncCuda.cpp -I ./NvEncoder/ ./NvEncoder/*.cpp \
+    -I/usr/local/cuda-11.4/include -I/home/ash/src/orange/external/NvEncoder -I/home/ash/nvenc_api/include \
+    -L/usr/local/cuda-11.4/lib64/ \
+    -lcudart -lcuda -lnvcuvid -lnvidia-encode
+rm -f targets_nvenc/*.o

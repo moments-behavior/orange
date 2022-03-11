19595a863b8782a9e4ac5ae61a7cac7a36480ba0
Jinyao <lingerlyn@gmail.com>
2022-03-11 13:20:58 -0500

    syn mechnism

--- src/thread.h
+++ src/thread.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <time.h>
 
+
 inline float tick() {
 	struct timespec ts;
 	uint32_t res = clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -13,4 +14,16 @@ inline float tick() {
 	return ((float) ((ts.tv_sec * 1e9) + ts.tv_nsec)) / (float) 1.0e9;
 }
 
+
+// Increment value with a lock and return the previous value
+inline uint64_t sync_fetch_and_add(volatile uint64_t *x, uint64_t by) {
+	// NOTE(dd): we're using a gcc/clang compiler extension to do this
+	// because mutexes were for some reason slower
+	return __sync_fetch_and_add(x, by);
+}
+
+
 #endif //ORANGE_THREADS
+
+
+

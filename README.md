272bf13c3ceabfc0c1fca9a23cda48e2855cb15f
Jinyao Yan <lingerlyn@example.com>
2022-06-14 11:58:44 -0400

    use shared ffmpeg library

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Encoding performance using GPU A6000 with 7MP Emergent camera
 
 ![encoding_benchmark](images/encoding_benchmark.png)
 Run `build.sh` in local folder
+
+
+## Build instructions 
--- build.sh
+++ build.sh
@@ -5,14 +5,13 @@ g++ -Ofast -ffast-math -std=c++14 \
     -o targets/*.o \
     -o targets/orange -I ./src/ src/*.cpp \
     -I./src/NvEncoder/ ./src/NvEncoder/*.cpp \
-    -I./nvenc_api/include -I/usr/include/opencv4 -I/opt/EVT/eSDK/include/ -I/usr/lib/x86_64-linux-gnu/gstreamer-1.0 -I/usr/local/cuda-11.4/include \
+    -I./nvenc_api/include -I/opt/EVT/eSDK/include/ -I/usr/local/cuda/include \
     -L/opt/EVT/eSDK/lib/ -lEmergentCamera  -lEmergentGenICam  -lEmergentGigEVision \
     -lm \
     -lpthread \
-    -lopencv_core -lopencv_imgcodecs -lopencv_bgsegm -lopencv_imgproc -lopencv_video -lopencv_highgui -lopencv_videoio \
-    -lgstreamer-1.0 \
-    -L/usr/local/cuda-11.4/lib64/ -lcudart -lcuda -lnppicc -lnvidia-encode \
+    -L/usr/local/cuda/lib64/ -lcudart -lcuda -lnppicc -lnvidia-encode \
     -lGLEW -lGLU -lGL \
-    `pkg-config --cflags --libs x11` \
-    `PKG_CONFIG_PATH=/path/ffmpeg/lib/pkgconfig/ pkg-config --cflags libavformat libswscale libswresample libavutil libavcodec` `PKG_CONFIG_PATH=/path/ffmpeg/lib/pkgconfig/ pkg-config --libs libavformat libswscale libswresample libavutil libavcodec`
+    -I/home/user/nvidia/ffmpeg/build/include/ \
+    -L/home/user/nvidia/ffmpeg/build/lib/ -lavformat -lswscale -lswresample -lavutil -lavcodec \
+    `pkg-config --cflags --libs x11`
 sudo ./targets/orange;

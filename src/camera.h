e2f801ba2ef1d55e57734664288469535285c505
Jinyao <lingerlyn@gmail.com>
2023-01-19 15:44:44 -0500

    dynamical allocation

--- src/camera.h
+++ src/camera.h
@@ -56,7 +56,6 @@ struct PTPParams{
 
 
 CameraParams create_camera_params(unsigned int width, unsigned int height, unsigned int frame_rate, unsigned int gain, unsigned int exposure, string pixel_format, string color_temp, int camera_id, int gpu_id, int num_cameras, bool gpu_direct);
-int check_cameras(int max_cameras, GigEVisionDeviceInfo *device_info, GigEVisionDeviceInfo *ordered_device_info);
 void configure_factory_defaults(Emergent::CEmergentCamera* camera);
 void close_camera(Emergent::CEmergentCamera* camera);
 void open_camera_with_params(Emergent::CEmergentCamera* camera, GigEVisionDeviceInfo* device_info, CameraParams* camera_params);
@@ -67,14 +66,7 @@ void ptp_camera_sync(Emergent::CEmergentCamera* camera);
 void quick_print_camera(GigEVisionDeviceInfo* device_info, int camera_idx);
 unsigned long long get_current_PTP_time(Emergent::CEmergentCamera* camera);
 void test_gpo_manual_toggle(Emergent::CEmergentCamera* camera);
-int get_camera_id(char* ip_address);
-void change_camera_ip(GigEVisionDeviceInfo* device_info, int camera_idx, const char* new_ip);
 void change_camera_ip_persistent(GigEVisionDeviceInfo* device_info, Emergent::CEmergentCamera* camera, const char* new_ip);
-void set_temporary_camera_ip(GigEVisionDeviceInfo* device_info, int num_camera);
-void set_rigroom_camera_ip_persistent(GigEVisionDeviceInfo* device_info, Emergent::CEmergentCamera* camera, int num_camera);
-void set_ip_persistent_with_open_close_camera(GigEVisionDeviceInfo* device_info, int num_camera);
-int order_for_test_rig(int max_cameras, GigEVisionDeviceInfo *device_info, GigEVisionDeviceInfo *ordered_device_info);
-int order_4_ceiling_cameras(int max_cameras, GigEVisionDeviceInfo *device_info, GigEVisionDeviceInfo *ordered_device_info);
 void update_gain_value(Emergent::CEmergentCamera* camera, int gain_val, CameraParams* camera_params);
 void update_exposure_value(Emergent::CEmergentCamera* camera, int exposure_val, CameraParams* camera_params);
 void update_frame_rate_value(Emergent::CEmergentCamera* camera, int frame_rate_val, CameraParams* camera_params);

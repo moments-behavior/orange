2a58ce817dceca96b7fb0c2c49b484d565a67252
Jinyao <lingerlyn@gmail.com>
2023-01-25 15:41:19 -0500

    refactor ptp

--- src/video_capture_gpu.h
+++ src/video_capture_gpu.h
@@ -61,6 +61,32 @@ struct EncoderContext
     NvEncoderCuda *pEnc;
 };
 
-void aquire_frames_gpu_encode(CameraEmergent *ecam, CameraParams *camera_params, CameraControl *camera_control, unsigned char *display_buffer, string encoder_setup, string folder_name);
+
+struct PTPState 
+{
+    int ptp_offset;
+    int ptp_offset_sum=0;
+    int ptp_offset_prev=0;
+    unsigned int ptp_time_low;
+    unsigned int ptp_time_high;
+    unsigned int ptp_time_plus_delta_to_start_low;
+    unsigned int ptp_time_plus_delta_to_start_high;
+    unsigned long long ptp_time_delta_sum = 0;
+    unsigned long long ptp_time_delta;
+    unsigned long long ptp_time;
+    unsigned long long ptp_time_prev;
+    unsigned long long ptp_time_countdown;
+    unsigned long long frame_ts; 
+    unsigned long long frame_ts_prev;
+    unsigned long long frame_ts_delta;
+    unsigned long long frame_ts_delta_sum = 0;
+    unsigned long long ptp_time_plus_delta_to_start;
+    char ptp_status[100];
+    unsigned long ptp_status_sz_ret;
+    unsigned int ptp_time_plus_delta_to_start_uint;
+};
+
+
+void sync_aquire_frames_gpu_encode(CameraEmergent *ecam, CameraParams *camera_params, CameraControl *camera_control, unsigned char *display_buffer, string encoder_setup, string folder_name, PTPParams* ptp_params);
 void aquire_frames_gpu(CameraEmergent *ecam, CameraParams *camera_params, CameraControl *camera_state, unsigned char *display_buffer);
 #endif

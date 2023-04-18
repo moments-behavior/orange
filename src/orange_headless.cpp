c423034eed210efb30bc45853d806f14d46f3915
Jinyao Yan <lingerlyn@gmail.com>
2023-04-18 12:00:58 -0400

    take laser videos

--- src/orange_headless.cpp
+++ src/orange_headless.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     for (int i = 0; i < num_cameras; i++)
     {
         int gpu_id = i%4;
-        init_7MP_camera_params_color(&cameras_params[i], i, num_cameras, 2000, 3500, gpu_id, frame_rate); 
+        init_7MP_camera_params_color(&cameras_params[i], i, num_cameras, 2000, 1000, gpu_id, frame_rate); 
         open_camera_with_params(&ecams[i].camera, &device_info[cameras_params[i].camera_id], &cameras_params[i]);
 
         int ReturnVal = 0;  

fe3edeefc238030862f72103ef1005304895228f
Jinyao Yan <lingerlyn@gmail.com>
2023-05-15 17:48:36 -0400

    refactor calibration

--- src/orange.cpp
+++ src/orange.cpp
@@ -9,11 +9,10 @@
 #include <imfilebrowser.h>
 #include "project.h"
 #include "gui.h"
-#include "camera_calibration.h"
+#include "calibration_gui.h"
 
 int main(int argc, char **args)
 {
-
     gx_context *window = (gx_context *)malloc(sizeof(gx_context));
     *window = (gx_context){
         .swap_interval = 1, // use vsync
@@ -54,38 +53,12 @@ int main(int argc, char **args)
     std::string encoder_preset = "p1";
     std::string folder_name;
 
-    // for camera calibration
-    
-    // allocate display buffer on cpu, make this dynamic later
-    int output_channels = 3;
-    int size_pic = 3208 * 2200 * output_channels * sizeof(unsigned char);
-    PictureBuffer display_buffer_cpu[8];
-    for(int j=0; j<8; j++){
-        display_buffer_cpu[j].frame = (unsigned char*)malloc(size_pic);
-        clear_buffer_with_constant_image(display_buffer_cpu[j].frame, 3208, 2200);
-        display_buffer_cpu[j].frame_number = 0;
-        display_buffer_cpu[j].available_to_write = true;
-    }
+    CPURender cpu_buffers;
+    CameraCalibResults calib_results;
+    CalibData calib_data;
 
-    GLuint texture[8];
-
-    for(int j=0; j<8; j++){
-        glGenTextures(1, &texture[j]);
-        glBindTexture(GL_TEXTURE_2D, texture[j]);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 3208, 2200, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-        // Setup filtering parameters for display
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
-    }
-
-    Settings calib_setting;
-    vector<vector<vector<Point2f>>> imagePoints;
-    vector<Mat> camera_matrices;
-    vector<Mat> dist_coeffs;
-    vector<int> image_save_index;
-    bool* selected_images_to_save;
+    bool show_calibration_window = false;
+    bool show_arucomarker_window = false;
 
     while (!glfwWindowShouldClose(window->render_target))
     {
@@ -212,7 +185,7 @@ int main(int argc, char **args)
 
                     for (int i = 0; i < num_cameras; i++)
                     {
-                        camera_threads.push_back(std::thread(&aquire_frames_gpu, &ecams[i], &cameras_params[i], camera_control, tex[i].display_buffer, encoder_setup, folder_name, ptp_params, &display_buffer_cpu[i]));
+                        camera_threads.push_back(std::thread(&aquire_frames_gpu, &ecams[i], &cameras_params[i], camera_control, tex[i].display_buffer, encoder_setup, folder_name, ptp_params, &cpu_buffers.display_buffer[i]));
                     }
 
                 } else {
@@ -333,7 +306,7 @@ int main(int argc, char **args)
                     for (int i = 0; i < num_cameras; i++)
                     {
                         encoder_setup = encoder_basic_setup + to_string(cameras_params[i].frame_rate);
-                        camera_threads.push_back(std::thread(&aquire_frames_gpu, &ecams[i], &cameras_params[i], camera_control, tex[i].display_buffer, encoder_setup, folder_name, ptp_params, &display_buffer_cpu[i]));
+                        camera_threads.push_back(std::thread(&aquire_frames_gpu, &ecams[i], &cameras_params[i], camera_control, tex[i].display_buffer, encoder_setup, folder_name, ptp_params, &cpu_buffers.display_buffer[i]));
                     }
                     camera_control->subscribe = true;                    
                 } else {
@@ -407,301 +380,17 @@ int main(int argc, char **args)
             }
         }
 
-
-        if (ImGui::Begin("Calibration"))
-        {
-            if (ImGui::Button("Load config file")){
-                const string inputSettingsFile = "/home/user/src/orange/circle.xml";
-                FileStorage fs(inputSettingsFile, FileStorage::READ);
-                if (!fs.isOpened())
-                {
-                    std::cout << "Could not open the configuration file: \"" << inputSettingsFile << "\"" << std::endl;
-                    return -1;
-                } 
-                fs["Settings"] >> calib_setting;
-                fs.release();
-
-                if (!calib_setting.goodInput)
-                {
-                    cout << "Invalid input detected. Application stopping. " << endl;
-                    return -1;
-                } else {
-                    std::cout << "Calibration configuration file loaded: \"" << inputSettingsFile << "\"" << std::endl;
-                }
-                camera_control->copy_to_cpu = true;
-                camera_control->calibration = true;
-
-                for(int i=0; i < num_cameras; i++) {
-                    vector<vector<Point2f>> image_points_per_cam;
-                    imagePoints.push_back(image_points_per_cam);
-                }
-
-
-                for(int i=0; i < num_cameras; i++){
-
-                    Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
-                    // initialization 
-                    if (!calib_setting.intrinsicGuess) {
-                        if( !calib_setting.useFisheye && calib_setting.flag & CALIB_FIX_ASPECT_RATIO )
-                            cameraMatrix.at<double>(0,0) = calib_setting.aspectRatio;
-                    } else {
-                        cameraMatrix = (Mat_<double>(3,3) << 2800, 0, 1100, 0, 2800, 1600, 0, 0, 1);
-                    }
-                    camera_matrices.push_back(cameraMatrix);
-
-                    if (calib_setting.useFisheye) {
-                        Mat distCoeffs = Mat::zeros(4, 1, CV_64F);
-                        dist_coeffs.push_back(distCoeffs);
-                    } else {
-                        Mat distCoeffs = Mat::zeros(8, 1, CV_64F);
-                        dist_coeffs.push_back(distCoeffs);
-                    }
-                }
-
-                for(int i=0; i < num_cameras; i++){
-                    image_save_index.push_back(0);
-                }
-                selected_images_to_save = new bool[num_cameras];
-                for(int i=0; i < num_cameras; i++){
-                    selected_images_to_save[i] = false;
-                }
-            }
-
-            
-            if (camera_control->calibration) {
-
-                if (ImGui::Button("Save images all")) 
-                {
-                    for(int i=0; i < num_cameras; i++){
-                        display_buffer_cpu[i].available_to_write = false;
-                    }
-                    
-                    for(int i=0; i < num_cameras; i++) {
-                        cv::Mat view = cv::Mat(3208 * 2200 * 3, 1, CV_8U, display_buffer_cpu[i].frame).reshape(3, 2200);
-                        string image_name = "/home/user/Calibration/8ceiling_cam_matlab/Cam" + std::to_string(cameras_params[i].camera_id) + "/image_" + std::to_string(image_save_index[i]) + ".tiff"; 
-                        cv::imwrite(image_name, view);
-                        image_save_index[i]++;
-                    }
-
-                    for(int i=0; i < num_cameras; i++){
-                        display_buffer_cpu[i].available_to_write = true;
-                    }
-                }
-               
-               
-                for(int i=0; i < num_cameras; i++) {
-                    ImGui::InputInt("Saving image index: ", &image_save_index[i]);
-                }
-
-                for(int i=0; i < num_cameras; i++) {
-                    char label[32];
-                    sprintf(label, "Cam%d", i);
-                    ImGui::Checkbox(label, &selected_images_to_save[i]);
-                    ImGui::SameLine();
-                }
-
-                if (ImGui::Button("Save selected")) 
-                {
-                    for(int i=0; i < num_cameras; i++){
-                        display_buffer_cpu[i].available_to_write = false;
-                    }
-                    
-                    for(int i=0; i < num_cameras; i++) {
-                        if (selected_images_to_save[i]) {
-                            cv::Mat view = cv::Mat(3208 * 2200 * 3, 1, CV_8U, display_buffer_cpu[i].frame).reshape(3, 2200);
-                            string image_name = "/home/user/Calibration/16cam_matlab/Cam" + std::to_string(cameras_params[i].camera_id) + "/image_" + std::to_string(image_save_index[i]) + ".tiff"; 
-                            cv::imwrite(image_name, view);
-                            image_save_index[i]++;
-                        }
-                    }
-
-                    for(int i=0; i < num_cameras; i++){
-                        display_buffer_cpu[i].available_to_write = true;
-                    }
-                }
-
-                
-                if (ImGui::Button("Detect")) 
-                {
-                    for(int i=0; i < num_cameras; i++){
-                        display_buffer_cpu[i].available_to_write = false;
-                    }
-                    
-                    for(int i=0; i < num_cameras; i++) {
-
-                        int winSize = 11;  // Half of search window for cornerSubPix
-                        // local the frame and process frame 
-                        cv::Mat view = cv::Mat(3208 * 2200 * 3, 1, CV_8U, display_buffer_cpu[i].frame).reshape(3, 2200);
-
-                        //! [find_pattern]
-                        vector<Point2f> pointBuf;
-                        bool found;
-                        int chessBoardFlags = CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK;
-
-                        switch(calib_setting.calibrationPattern) // Find feature points on the input format
-                        {
-                            case Settings::CHESSBOARD:
-                                found = findChessboardCorners(view, calib_setting.boardSize, pointBuf);
-                                break;
-                            case Settings::CIRCLES_GRID:
-                                found = findCirclesGrid(view, calib_setting.boardSize, pointBuf );
-                                break;
-                            case Settings::ASYMMETRIC_CIRCLES_GRID:
-                                found = findCirclesGrid(view, calib_setting.boardSize, pointBuf, CALIB_CB_ASYMMETRIC_GRID);
-                                std::cout << "here?" << std::endl;
-                                break;
-                            default:
-                                found = false;
-                                break;
-                        }
-
-                        std::cout << "\n after finding corner?:" << found << std::endl;
-                        //! [find_pattern]
-                        //! [pattern_found]
-                        if (found)                // If done with success,
-                        {
-                            // improve the found corners' coordinate accuracy for chessboard
-                                if(calib_setting.calibrationPattern == Settings::CHESSBOARD)
-                                {
-                                    Mat viewGray;
-                                    cvtColor(view, viewGray, COLOR_BGR2GRAY);
-                                    cornerSubPix( viewGray, pointBuf, Size(winSize,winSize),
-                                        Size(-1,-1), TermCriteria(TermCriteria::EPS+TermCriteria::COUNT, 30, 0.0001));
-                                }
-                                
-                                imagePoints[i].push_back(pointBuf);                                
-                                std::cout << pointBuf << std::endl;
-                                // Draw the corners.
-                                drawChessboardCorners(view, calib_setting.boardSize, Mat(pointBuf), found);
-                                bitwise_not(view, view);
-                        }
-                    }
-
-                }
-
-                if (ImGui::Button("Get new frame")) {
-                    for(int i=0; i < num_cameras; i++){
-                        display_buffer_cpu[i].available_to_write = true;
-                    }
-                }
-
-                for(int i=0; i < num_cameras; i++) {
-                    int no_frames = imagePoints[i].size();
-                    std::string no_frames_str = "Number of Frames: " + std::to_string(no_frames);
-                    if(no_frames < 25)
-                    {
-                        ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), no_frames_str.c_str());
-                    } else {
-                        ImGui::TextColored(ImVec4(0.0f, 1.0f, 1.0f, 1.0f), no_frames_str.c_str());
-                    }
-                }
-                
-                if(ImGui::Button("Run calibration"))
-                {
-                    float grid_width = calib_setting.squareSize * (calib_setting.boardSize.width - 1);
-                    Size imageSize = cv::Size(2200, 3200);
-                    cout << "imageSize" << imageSize << endl;
-
-                    for(int i=0; i < num_cameras; i++){
-                        string cam_calib_out = "Cam" + std::to_string(cameras_params[i].camera_id) + ".xml"; 
-                        if(runCalibrationAndSave(cam_calib_out, calib_setting, imageSize, camera_matrices[i], dist_coeffs[i], imagePoints[i], grid_width, false)) {
-                            printf("Calibrated");
-                        }                                        
-                    } 
-                }
-
-                if(ImGui::Button("Load intrinsics")){
-                    for(int i=0; i < num_cameras; i++){
-                        string input_intrinsic_files = "Cam" + std::to_string(cameras_params[i].camera_id) + ".xml"; 
-                        loadIntrinsics(input_intrinsic_files, camera_matrices[i], dist_coeffs[i]);
-                    }
-                }
-
-                if(ImGui::Button("Estimate camera pose")){
-                    
-                    // detect 
-                    for(int i=0; i < num_cameras; i++){
-                        display_buffer_cpu[i].available_to_write = false;
-                    }
-                    
-                    for(int i=0; i < num_cameras; i++) 
-                    {
-                        int winSize = 11;  // Half of search window for cornerSubPix
-                        // local the frame and process frame 
-                        cv::Mat view = cv::Mat(3208 * 2200 * 3, 1, CV_8U, display_buffer_cpu[i].frame).reshape(3, 2200);
-
-                        //! [find_pattern]
-                        vector<Point2f> pointBuf;
-                        bool found;
-                        int chessBoardFlags = CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK;
-
-                        switch(calib_setting.calibrationPattern) // Find feature points on the input format
-                        {
-                            case Settings::CHESSBOARD:
-                                found = findChessboardCorners(view, calib_setting.boardSize, pointBuf);
-                                break;
-                            case Settings::CIRCLES_GRID:
-                                found = findCirclesGrid(view, calib_setting.boardSize, pointBuf );
-                                break;
-                            case Settings::ASYMMETRIC_CIRCLES_GRID:
-                                found = findCirclesGrid(view, calib_setting.boardSize, pointBuf, CALIB_CB_ASYMMETRIC_GRID);
-                                std::cout << "here?" << std::endl;
-                                break;
-                            default:
-                                found = false;
-                                break;
-                        }
-
-                        std::cout << "\n after finding corner?:" << found << std::endl;
-                        //! [find_pattern]
-                        //! [pattern_found]
-                        if (found)                // If done with success,
-                        {
-                            // improve the found corners' coordinate accuracy for chessboard
-                                if(calib_setting.calibrationPattern == Settings::CHESSBOARD)
-                                {
-                                    Mat viewGray;
-                                    cvtColor(view, viewGray, COLOR_BGR2GRAY);
-                                    cornerSubPix(viewGray, pointBuf, Size(winSize,winSize),
-                                        Size(-1,-1), TermCriteria(TermCriteria::EPS+TermCriteria::COUNT, 30, 0.0001));
-                                }
-                                imagePoints[i].push_back(pointBuf);                                
-                                // Draw the corners.
-                                drawChessboardCorners(view, calib_setting.boardSize, Mat(pointBuf), found);
-                                bitwise_not(view, view);
-                                
-                                string cam_calib_estrinsics = "Cam" + std::to_string(cameras_params[i].camera_id) + "_extrinsics.xml"; 
-                                // estimate extrinsics 
-                                if(estimatePose(cam_calib_estrinsics, calib_setting, pointBuf, camera_matrices[i], dist_coeffs[i], SOLVEPNP_ITERATIVE)){
-                                    std::cout << "Extrinsics estimated successfully." << std::endl;
-                                }
-                        }
-                    }
-                }
-
-                for(int j=0; j<num_cameras; j++){
-                    // if the current frame is ready, upload for display, otherwise wait for the frame to get ready 
-                    bind_texture(&texture[j]);
-                    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 3208, 2200, GL_RGB, GL_UNSIGNED_BYTE, display_buffer_cpu[j].frame);
-                    unbind_texture();
-                }
-
-                for (int i = 0; i < num_cameras; i++) {
-                    string window_name = "Cam" + std::to_string(cameras_params[i].camera_id);            
-                    ImGui::Begin(window_name.c_str());
-                    ImVec2 avail_size = ImGui::GetContentRegionAvail();
-
-                    //ImGui::Image((void*)(intptr_t)texture[i], avail_size);
-                    if (ImPlot::BeginPlot("##no_plot_name", avail_size)){
-                        ImPlot::PlotImage("##no_image_name", (void*)(intptr_t)texture[i], ImVec2(0,0), ImVec2(cameras_params[i].width, cameras_params[i].height));
-                        ImPlot::EndPlot();
-                    }
-
-                    ImGui::End();            
-                }
-            }
+        if (ImGui::Begin("Realtime Tools")) {
+            ImGui::Checkbox("Calibration", &show_calibration_window);      
+            ImGui::Checkbox("Aruco Marker", &show_arucomarker_window);
         }
         ImGui::End();
+
+
+        if (show_calibration_window) {
+            // calibration_window();
+        }
+
         render_a_frame(window);
 
     }

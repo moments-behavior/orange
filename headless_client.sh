b7793fe7194c29b9b7493dda6c93155922da3a89
lingerlyn <lingerlyn@gmail.com>
2023-09-28 12:12:30 -0400

    network

--- headless_client.sh
+++ headless_client.sh
@@ -4,7 +4,7 @@ rm -f targets2/orange_headless;
 nvcc -c src/kernel.cu -arch=sm_80 -o targets2/kernel.o
 
 g++ -Ofast -ffast-math -std=c++17 targets2/*.o \
-    -o targets2/orange_headless_client -I ./src/ src/orange_headless_client.cpp src/network_base.cpp src/camera_driver_helper.cpp src/camera.cpp src/video_capture.cpp \
+    -o targets2/orange_headless_client -I ./src/ src/orange_headless_client.cpp src/network_base.cpp \
     -I./src/NvEncoder/ ./src/NvEncoder/*.cpp \
     -I./nvenc_api/include -I/opt/EVT/eSDK/include/ -I/usr/local/cuda/include \
     -L/opt/EVT/eSDK/lib/ -lEmergentCamera  -lEmergentGenICam  -lEmergentGigEVision \

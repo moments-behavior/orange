f02fe3f21432f05297cdda08fa4c4dc8ac1995bb
lingerlyn <lingerlyn@gmail.com>
2023-12-08 19:11:28 -0500

    build with yolo

--- src/yolov8_pose.cpp
+++ src/yolov8_pose.cpp
@@ -1,63 +1,4 @@
-//
-//Adapted from https://github.com/triple-Mu/YOLOv8-TensorRT
-//
-
-#ifndef POSE_NORMAL_YOLOv8_pose_HPP
-#define POSE_NORMAL_YOLOv8_pose_HPP
-
-#include "NvInferPlugin.h"
-#include "common.hpp"
-#include "fstream"
-#include <nppi.h>
-
-using namespace pose;
-
-class YOLOv8_pose {
-public:
-    explicit YOLOv8_pose(const std::string& engine_file_path);
-
-    ~YOLOv8_pose();
-
-    void make_pipe(bool warmup = true);
-
-    void copy_from_Mat(const cv::Mat& image);
-
-    void copy_from_Mat(const cv::Mat& image, cv::Size& size);
-
-    void preprocess_gpu(unsigned char* d_rgb, unsigned char* temp, unsigned char* boarder_image, float* float_image, float* float_planenar, cv::Size& image_size, cv::Size& size);
-
-    void letterbox(const cv::Mat& image, cv::Mat& out, cv::Size& size);
-
-    void copy_keypoints_gpu(float* d_points, const std::vector<Object>& objs);
-
-    void infer();
-
-    void postprocess(std::vector<Object>& objs, float score_thres = 0.25f, float iou_thres = 0.65f, int topk = 100);
-
-    static void draw_objects(const cv::Mat&                                image,
-                             cv::Mat&                                      res,
-                             const std::vector<Object>&                    objs,
-                             const std::vector<std::vector<unsigned int>>& SKELETON,
-                             const std::vector<std::vector<unsigned int>>& KPS_COLORS,
-                             const std::vector<std::vector<unsigned int>>& LIMB_COLORS);
-
-    int                  num_bindings;
-    int                  num_inputs  = 0;
-    int                  num_outputs = 0;
-    std::vector<Binding> input_bindings;
-    std::vector<Binding> output_bindings;
-    std::vector<void*>   host_ptrs;
-    std::vector<void*>   device_ptrs;
-
-    PreParam pparam;
-    cudaStream_t                 stream  = nullptr;
-
-private:
-    nvinfer1::ICudaEngine*       engine  = nullptr;
-    nvinfer1::IRuntime*          runtime = nullptr;
-    nvinfer1::IExecutionContext* context = nullptr;
-    Logger                       gLogger{nvinfer1::ILogger::Severity::kERROR};
-};
+#include "yolov8_pose.h"
 
 YOLOv8_pose::YOLOv8_pose(const std::string& engine_file_path)
 {
@@ -487,4 +428,3 @@ void YOLOv8_pose::draw_objects(const cv::Mat&                                ima
     }
 }
 
-#endif  // POSE_NORMAL_YOLOv8_pose_HPP

3a8b859468c61e2a8ec0288033ab960d8ed527cc
Jinyao Yan <lingerlyn@gmail.com>
2024-10-31 14:45:48 -0400

    edit docs

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-sudo LD_LIBRARY_PATH=/usr/local/cuda/lib64:$HOME/nvidia/ffmpeg/build/lib:$HOME/build/TensorRT-8.6.1.6/lib ./targets/orange
+DIR_FFMPEG=$HOME/nvidia/ffmpeg
+DIR_TENSORRT=$HOME/nvidia/ffmpeg/TensorRT-8.6.1.6
+sudo LD_LIBRARY_PATH=/usr/local/cuda/lib64:$DIR_FFMPEG/build/lib:$DIR_TENSORRT/lib ./targets/orange

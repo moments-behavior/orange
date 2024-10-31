41603fd127092cb2d0419e2eb9a83cbf31d172ca
lingerlyn <lingerlyn@gmail.com>
2024-10-31 18:17:54 -0400

    fix dir path in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 DIR_FFMPEG=$HOME/nvidia/ffmpeg
-DIR_TENSORRT=$HOME/nvidia/ffmpeg/TensorRT-8.6.1.6
+DIR_TENSORRT=$HOME/nvidia/TensorRT-8.6.1.6
 sudo LD_LIBRARY_PATH=/usr/local/cuda/lib64:$DIR_FFMPEG/build/lib:$DIR_TENSORRT/lib ./targets/orange

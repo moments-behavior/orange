d42f98362f4e5a015763ddd189583b1772a70e94
Ratan Othayoth <ratan@janelia.hhmi.org>
2025-02-24 15:44:44 -0500

    updated run script and engine files

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-DIR_FFMPEG=$HOME/nvidia/ffmpeg
-DIR_TENSORRT=$HOME/nvidia/TensorRT
+DIR_FFMPEG=$HOME/build/FFmpeg
+DIR_TENSORRT=$HOME/build/TensorRT-10.6.0.26
 sudo LD_LIBRARY_PATH=/usr/local/cuda/lib64:$DIR_FFMPEG/build/lib:$DIR_TENSORRT/lib ./targets/orange

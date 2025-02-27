9a5156b284cb47b3941fa8bbd38e461d802a6ece
lingerlyn <lingerlyn@gmail.com>
2025-02-27 09:59:56 -0500

    reorganize benchmeark

--- build_benchmark.sh
+++ build_benchmark.sh
@@ -1,10 +1,10 @@
-mkdir -p tools;
-nvcc -c src/kernel.cu -arch=sm_80 -o tools/kernel.o
+mkdir -p targets3;
+nvcc -c src/kernel.cu -arch=sm_80 -o targets3/kernel.o
 
 DIR_TENSORRT=$HOME/nvidia/TensorRT
 
-g++ -Ofast -ffast-math -std=c++17 tools/*.o -o tools/yolo_tester \
-    -I./src/ src/yolo_offline_tester.cpp src/yolov8_det.cpp \
+g++ -Ofast -ffast-math -std=c++17 targets3/*.o -o targets3/yolo_tester \
+    -I./src/ benchmark/yolo_offline_tester.cpp src/yolov8_det.cpp \
     -I/usr/local/include/opencv4 \
     -lopencv_sfm -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_videoio -lopencv_highgui -lopencv_video -lopencv_calib3d \
     -I$DIR_TENSORRT/include -L$DIR_TENSORRT/lib/ -lnvinfer -lnvinfer_plugin \

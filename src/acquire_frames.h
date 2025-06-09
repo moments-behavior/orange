cd34619036b3140ceaf44c01d78fe08141a125b7
jmdelahanty <delahantyj@janelia.hhmi.org>
2025-06-09 15:51:19 -0400

    streaming and yolo works on separate threads, but latency in display it seems

--- src/acquire_frames.h
+++ src/acquire_frames.h
@@ -2,5 +2,19 @@
 #define ORANGE_ACQUIRE_FRAMES
 #include "video_capture.h"
 #include "yolo_worker.h"
-void acquire_frames(CameraEmergent *ecam, CameraParams *camera_params, CameraEachSelect* camera_select, CameraControl* camera_control, unsigned char *display_buffer, std::string encoder_setup, std::string folder_name, PTPParams* ptp_params, INDIGOSignalBuilder* indigo_signal_builder, YOLOv8Worker* yolo_worker_for_this_camera);
+#include "thread.h"
+#include "image_processing.h"
+void acquire_frames(CameraEmergent *ecam,
+    CameraParams *camera_params,
+    CameraEachSelect* camera_select,
+    CameraControl* camera_control,
+    unsigned char *display_buffer,
+    std::string encoder_setup,
+    std::string folder_name,
+    PTPParams* ptp_params,
+    INDIGOSignalBuilder* indigo_signal_builder,
+    YOLOv8Worker* yolo_worker_for_this_camera,
+    SafeQueue<WORKER_ENTRY*>* free_entries_queue,
+    SafeQueue<WORKER_ENTRY*>* recycle_queue
+    );
 #endif

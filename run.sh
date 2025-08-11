381ee797977405dbd347b6fd7aea03cadaccbc36
lingerlyn <lingerlyn@gmail.com>
2025-08-11 09:29:52 -0400

    default to cmake build

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-DIR_FFMPEG=$HOME/nvidia/ffmpeg
-DIR_TENSORRT=$HOME/nvidia/TensorRT
-sudo LD_LIBRARY_PATH=/usr/local/cuda/lib64:$DIR_FFMPEG/build/lib:$DIR_TENSORRT/lib ./targets/orange
+sudo release/orange

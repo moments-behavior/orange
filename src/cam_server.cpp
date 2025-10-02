7a64d4153d82f1f438fec2f9606cf4b266283e7c
lingerlyn <lingerlyn@gmail.com>
2025-10-02 14:25:28 -0400

    reset last command

--- src/cam_server.cpp
+++ src/cam_server.cpp
@@ -353,40 +353,40 @@ static void server_on_event(const Incoming &evt) {
 }
 
 static void cleanup_host_server_resources() {
-    if (ptp_params.network_set_stop_ptp && ptp_params.ptp_stop_reached) {
-        ptp_params.network_set_stop_ptp = false;
-        for (auto &t : camera_threads)
-            t.join();
+    ptp_params.network_set_stop_ptp = false;
+    for (auto &t : camera_threads)
+        t.join();
 
-        for (int i = 0; i < cam_count; i++) {
-            camera_threads.pop_back();
-        }
-
-        for (int i = 0; i < cam_count; i++) {
-            ptp_sync_off(&ecams[i].camera, &cameras_params[i]);
-        }
-        ptp_params.ptp_global_time = 0;
-        ptp_params.ptp_stop_time = 0;
-        ptp_params.ptp_counter = 0;
-        ptp_params.ptp_stop_counter = 0;
-        ptp_params.network_sync = true;
-        ptp_params.network_set_start_ptp = false;
-        ptp_params.ptp_stop_reached = false;
-        ptp_params.ptp_start_reached = false;
-        camera_control.sync_camera = false;
-
-        for (int i = 0; i < cam_count; i++) {
-            destroy_frame_buffer(&ecams[i].camera, ecams[i].evt_frame,
-                                 evt_buffer_size, &cameras_params[i]);
-            delete[] ecams[i].evt_frame;
-            check_camera_errors(EVT_CameraCloseStream(&ecams[i].camera),
-                                cameras_params[i].camera_serial.c_str());
-            close_camera(&ecams[i].camera, &cameras_params[i]);
-        }
-        ecams.clear();
-        cameras_params.clear();
-        cameras_select.clear();
+    for (int i = 0; i < cam_count; i++) {
+        camera_threads.pop_back();
     }
+
+    for (int i = 0; i < cam_count; i++) {
+        ptp_sync_off(&ecams[i].camera, &cameras_params[i]);
+    }
+    ptp_params.ptp_global_time = 0;
+    ptp_params.ptp_stop_time = 0;
+    ptp_params.ptp_counter = 0;
+    ptp_params.ptp_stop_counter = 0;
+    ptp_params.network_sync = true;
+    ptp_params.network_set_start_ptp = false;
+    ptp_params.ptp_stop_reached = false;
+    ptp_params.ptp_start_reached = false;
+    camera_control.sync_camera = false;
+
+    for (int i = 0; i < cam_count; i++) {
+        destroy_frame_buffer(&ecams[i].camera, ecams[i].evt_frame,
+                             evt_buffer_size, &cameras_params[i]);
+        delete[] ecams[i].evt_frame;
+        check_camera_errors(EVT_CameraCloseStream(&ecams[i].camera),
+                            cameras_params[i].camera_serial.c_str());
+        close_camera(&ecams[i].camera, &cameras_params[i]);
+    }
+    ecams.clear();
+    cameras_params.clear();
+    cameras_select.clear();
+
+    g_last_done = camnet::v1::ServerControl_NONE;
 }
 
 int main(int argc, char **argv) {
@@ -410,7 +410,9 @@ int main(int argc, char **argv) {
         // events
         enet_dispatch_block(ctx.net, 5, server_on_event);
         // ... do per-tick housekeeping here if needed ...
-        cleanup_host_server_resources();
+        if (ptp_params.network_set_stop_ptp && ptp_params.ptp_stop_reached) {
+            cleanup_host_server_resources();
+        }
     }
     return 0;
 }

984b9a1d8781073aea4ace69680c75ffdc48f103
Jinyao Yan <lingerlyn@gmail.com>
2026-04-28 12:03:21 -0400

    Delete debug.sh

--- debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$(dirname "$(realpath "$0")")"
-mkdir -p cmake-build-debug
-/home/user/Downloads/clion-2024.3.1/bin/cmake/linux/x64/bin/cmake --build $DIR/cmake-build-debug --target all -j $(( $(nproc) - 2 ))
